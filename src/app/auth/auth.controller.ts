b78fc644101db487072b755149f406f735e4e1c2
cojack <xcojack@gmail.com>
2019-01-25 11:48:26 +0100

    chore(debug): add some logs to figure out what happen on fargate

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -18,6 +18,7 @@ import {Profile} from '../_helpers/decorators';
 import {config} from '../../config';
 import {Client, ClientProxy, Transport} from '@nestjs/microservices';
 import {USER_CMD_REGISTER} from '../user';
+import {AppLogger} from '../app.logger';
 
 @ApiUseTags('auth')
 @Controller('auth')
@@ -25,6 +26,8 @@ export class AuthController {
 
 	@Client({ transport: Transport.TCP })
 	private client: ClientProxy;
+
+	private logger = new AppLogger(AuthController.name);
 
 	constructor(
 		private readonly authService: AuthService,
@@ -37,6 +40,7 @@ export class AuthController {
 	@UseGuards(AuthGuard('jwt'))
 	@ApiResponse({ status: 200, description: 'OK', type: TokenDto })
 	public async verify(@Headers('Authorization') token: string): Promise<TokenDto> {
+		this.logger.debug(`[verify] Token ${token}`);
 		return verifyToken(token, config.session.secret);
 	}
 
@@ -44,6 +48,7 @@ export class AuthController {
 	@ApiResponse({ status: 200, description: 'OK', type: JwtDto })
 	public async login(@Body() credentials: CredentialsDto): Promise<JwtDto> {
 		const user = await this.userService.login(credentials);
+		this.logger.debug(`[login] User ${credentials.email} logging`);
 		return createToken(user);
 	}
 
@@ -52,7 +57,9 @@ export class AuthController {
 	@ApiResponse({ status: 200, description: 'OK', type: JwtDto })
 	public async register(@Body() data: DeepPartial<UserEntity>): Promise<JwtDto> {
 		const user = await this.userService.create(data);
+		this.logger.debug(`[register] User ${data.email} register`);
 		this.client.send({cmd: USER_CMD_REGISTER}, user).subscribe();
+		this.logger.debug(`[register] Send registration email for email ${data.email}`);
 		return createToken(user);
 	}
 
@@ -60,6 +67,7 @@ export class AuthController {
 	@ApiResponse({ status: 200, description: 'OK', type: JwtDto })
 	public async refreshToken(@Body() body: RefreshTokenDto): Promise<JwtDto> {
 		const token = await verifyToken(body.refreshToken, config.session.refresh.secret);
+		this.logger.debug(`[refresh] Token ${body.refreshToken}`);
 		return await createToken({id: token.id});
 	}
 
@@ -67,8 +75,10 @@ export class AuthController {
 	@UseGuards(AuthGuard('facebook-token'))
 	@ApiResponse({ status: 200, description: 'OK', type: JwtDto })
 	public async fbSignIn(@Body() fbToken: FacebookTokenDto, @Profile() profile: FacebookProfile): Promise<JwtDto> {
+		this.logger.debug(`[fbSignIn] Facebook socialId ${profile.id}`);
 		let user = await this.userService.findOne({filter: {...equals(profile.id), subject: 'socialId'}});
 		if (!user) {
+			this.logger.debug(`[fbSignIn] User with this id doesn't exists before, social register`);
 			user = await this.userService.socialRegister({
 				email: profile._json.email,
 				first_name: profile._json.first_name,
--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -5,15 +5,19 @@ import {UserEntity} from './entity';
 import {mail, renderTemplate} from '../_helpers/mail';
 import {USER_CMD_REGISTER} from './user.constants';
 import {config} from '../../config';
+import {AppLogger} from '../app.logger';
 
 @Controller()
 export class UserController {
+	private logger = new AppLogger(UserController.name);
+
 	constructor(protected service: UserService) {
 
 	}
 
 	@MessagePattern({ cmd: USER_CMD_REGISTER })
 	public async onUserRegister(user: UserEntity): Promise<void> {
+		this.logger.debug(`[onUserRegister] Send registration email for user ${user.email}`);
 		return mail({
 			subject: `Welcome ${user.first_name} to ${config.name}`,
 			to: user.email,
